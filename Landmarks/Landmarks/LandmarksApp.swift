6c16b3f860db9605285320a69b2238e6272d8936
tannguyen <mr.tannguyen@live.com>
2021-07-04 20:39:50 +0700

    first time

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tan Nguyen on 04/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
